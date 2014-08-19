7b91ab40812e5e63f77363eeb5ef19a82d0ddc21
FlicAnderson <contact@felicityanderson.co.uk>
2014-08-19 15:50:04 +0100

    changed the comment format slightly and updated the title information

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera :: R Programming :: Programming Assignment 2 :: cachematrix.R
+## ======================================================================== 
+## (19th August 2014)
+## Author: Flic Anderson
+##
 
-## Write a short comment describing this function
+## AIM: brief aim of this script & give an overall description of what your
+## ... functions do.
 
+# source functions:
+
+# ?
+
+
+# Write a short comment describing this function
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
-
+# Write a short comment describing this function
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
